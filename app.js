b05d06b9d22aa9b29b737702ea4ec17ff2f2a917
Nancy-Michael <134530795+Nancy-Michael@users.noreply.github.com>
2023-05-25 19:13:46 -0400

    updated files

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
+const addButton = document.getElementById('#addbutton');
+const listTodo = document.getElementById('#tasks');
+const deleteButton = document.getElementById('#dbtn');
 
-const myList = document.querySelector('#List');
-const button = document.querySelector('#btn');
-const listTodo = document.querySelector('#tasks');
-button.addEventListener('click', function () {
-    addTask();
-})
+let newArray = [];
 
 function addTask() {
-    const input = prompt('Add your new task!');
-    if (input === '') {
-        alert('You have add NOTHING to your list!!');
-    } else {
+    const input = prompt('Add your new task..');
+    let whitespace = /^[a-z][a-z\s]*$/;
+    if (input.match(whitespace)) {
+        for (let i = input; i < input.length; i++);
+        newArray.push(input);
         let li = document.createElement('li');
         li.innerHTML = input;
         listTodo.appendChild(li);
         let span = document.createElement('span');
         span.innerHTML = '&times';
         li.append(span);
+    } else {
+        alert('You have add Nothing to your list!!!');
     }
 }
+addButton.addEventListener('click', function () {
+    addTask();
+
+})
+
 listTodo.addEventListener('click', function (e) {
     if (e.target.tagName === 'LI') {
         e.target.classList.toggle('checked');
-    } else if (e.target.tagName === 'SPAN') {
+    }
+}, false);
+
+listTodo.addEventListener('click', function (e) {
+    if (e.target.tagName === 'SPAN') {
         e.target.parentElement.remove();
     }
 }, false);
+
